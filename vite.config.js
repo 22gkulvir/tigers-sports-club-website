2fe0a295f429ba181e412760733a7c67ccfe702f
22gkulvir <22gkulvir@gmail.com>
2026-09-20 14:30:27 -0500

    Update base path for custom domain deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tigers-sports-club-website/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
